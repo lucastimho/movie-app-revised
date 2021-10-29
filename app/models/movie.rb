34121cbfe37ae981315d0e6ab0b4d891d9c3f3f4
lucastimho <lucas.t.ho.lh@gmail.com>
2021-10-28 23:14:25 -0500

    adds movie_genre model and made appropriate associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, :plot, :director, length: {minimum: 2}
   validates :year, length: {maximum: 4}
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
